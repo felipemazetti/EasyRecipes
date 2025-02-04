889e9a30f8b9d1b902e79032ea76193a5499f221
Felipe Mazetti <felipemazetti@hotmail.com>
2025-02-04 16:23:56 -0300

    EasyRecipes update

---
+++ app/src/main/java/com/devspace/myapplication/RetrofitClient.kt
@@ -0,0 +1,34 @@
+package com.devspace.myapplication
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://api.spoonacular.com/"
+
+object RetrofitClient {
+
+    private val httpClient: OkHttpClient
+        get() {
+            val clientBuilder = OkHttpClient.Builder()
+            val token = BuildConfig.API_KEY
+
+            clientBuilder.addInterceptor { chain ->
+                val original: Request = chain.request()
+                val requestBuilder: Request.Builder = original.newBuilder()
+                val originalHttpUrl = chain.request().url
+                val newUrl = originalHttpUrl.newBuilder()
+                    .addQueryParameter("apiKey", token).build()
+                chain.proceed(requestBuilder.url(newUrl).build())
+            }
+            return clientBuilder.build()
+        }
+
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(httpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
